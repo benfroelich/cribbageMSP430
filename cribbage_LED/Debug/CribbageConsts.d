3f6565e5eb1e6f585f05f7043da0ce56ec8a852a
Benny <benfroelich@gmail.com>
2017-02-14 22:52:11 -0800

    resolved SO, migrated to CCSv7. Temp disabled ctpl

--- cribbage_LED/Debug/CribbageConsts.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-CribbageConsts.obj: ../CribbageConsts.cpp
-CribbageConsts.obj: ../cribbage_LED.h
-CribbageConsts.obj: /Applications/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/stdint.h
-
-../CribbageConsts.cpp: 
-../cribbage_LED.h: 
-/Applications/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/stdint.h: 
--- cribbage_LED/Debug/gameframe.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-gameframe.obj: ../gameframe.cpp
-gameframe.obj: ../gameframe.h
-gameframe.obj: ../crib_globals.h
-
-../gameframe.cpp: 
-../gameframe.h: 
-../crib_globals.h: 
